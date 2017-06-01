c73e2c80c1f9d7276398eb36ef8c00407651325c
Jamie Halvorson <jamie@halvorson.co.uk>
2017-06-01 06:58:42 +0100

    Setup webpack for JS+SCSS 💅

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const javascript = {
+  test: /\.(js)$/,
+  use: [{
+    loader: 'babel-loader',
+    options: { presets: ['es2015'] },
+  }],
+};
+
+const postcss = {
+  loader: 'postcss-loader',
+  options: {
+    plugins() { return [autoprefixer({ browsers: 'last 3 versions' })]; }
+  }
+};
+
+const styles = {
+  test: /\.(scss)$/,
+  use: ExtractTextPlugin.extract(['css-loader?sourceMap', postcss, 'sass-loader?sourceMap'])
+};
+
+const uglifiy = new webpack.optimize.UglifyJsPlugin({ // eslint-disable-line
+  compress: { warning: false }
+});
+
+const config = {
+  entry: {
+    App: './public/scripts/edinbar.js'
+  },
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'public', 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [javascript, styles]
+  },
+  // plugins: [uglify]
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ]
+};
+
+process.noDeprecation = true;
+
+module.exports = config;
